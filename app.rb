42f7e3e69301dca145b8c5a4b1031787c71acfa9
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-15 10:09:56 +0300

    Add rakefile

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ set :database, 'sqlite3:barbershop.db'
 
 class Client < ActiveRecord::Base
 
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
