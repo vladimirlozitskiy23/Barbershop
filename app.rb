d1331ca6ad78c6872bcbbfcd193bd4fc573b0ffa
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-15 18:41:55 +0300

    Add barbers migration

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, 'sqlite3:barbershop.db'
+# set :database, 'sqlite3:barbershop.db'
+set :database, {adapter: "sqlite3", database: "barbershop.db.sqlite3"}
 
 class Client < ActiveRecord::Base
+end
 
+class Barber < ActiveRecord::Base
 end
 
 get '/' do
---
+++ db/migrate/20230215152808_create_barbers.rb
@@ -0,0 +1,12 @@
+class CreateBarbers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :barbers do |t|
+      t.text :name
+
+      t.timestamps
+    end
+    Barber.create :name => 'Jessie Pincman'
+    Barber.create :name => 'Walter White'
+    Barber.create :name => 'Gus Fring'
+  end
+end
