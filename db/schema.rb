3f729e3dac64f38fc7a85a9421a7f78e40f3b8ce
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-20 16:47:11 +0300

    Modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_152808) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_102848) do
   create_table "barbers", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_152808) do
     t.text "datestamp"
     t.text "barber"
     t.text "color"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "email"
+    t.text "contact"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
