9d5c59988bf62ecaed651db328b13fd6717711ee
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-20 12:07:17 +0300

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_152808) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text "name"
     t.text "phone"
     t.text "datestamp"
