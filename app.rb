7a84c77c742ae1e0387500d07bdcef89610e3cc7
vladimirlozitskiy23 <rubyonrails2023@gmail.com>
2023-02-20 16:54:17 +0300

    create Contact model,create get and post visit,create get and post contacts

--- app.rb
+++ app.rb
@@ -13,7 +13,40 @@ end
 class Barber < ActiveRecord::Base
 end
 
+class Contact < ActiveRecord::Base
+end
+
+before do
+	@barbers = Barber.all
+
+end
+
 get '/' do
 	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit' do
+
+	erb :visit
+end
+
+
+post '/visit' do
+	c = Client.new params[:client]
+	c.save
+		erb "<h3>Thanks! You registred!</h3>"
+
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	c = Contact.new params[:contact]
+	c.save
+
+	erb :contacts
+
 end
